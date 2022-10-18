282c8d2a8a15fe8b7e73c3f89da3bdc5c8bd69dd
anton19ds <bar2018ka@gmail.com>
2022-10-18 16:57:52 +0300

    icon9

--- widgets/views/cities.php
+++ widgets/views/cities.php
@@ -1,17 +1,32 @@
 <div class="item item_city">
   <a class="current" href="">
-    <? if(isset($nameThisCity) && !empty($nameThisCity)):?>
+    <? if (isset($nameThisCity) && !empty($nameThisCity)) : ?>
       <?= $nameThisCity ?>
-      <? endif;?>
+    <? endif; ?>
   </a>
 </div>
 <? if (isset($kontakty['phone']) && !empty($kontakty['phone'])) : ?>
   <div class="item item_phone">
-  <a href="tel:<?= $kontakty['phone'] ?>" class="fil"><?= $kontakty['phone'] ?></a>
-</div>
+    <a href="tel:<?= $kontakty['phone'] ?>" class="fil"><?= $kontakty['phone'] ?></a>
+  </div>
 <? endif; ?>
 <div class="item social">
-<? if (isset($kontakty['youtube']) && !empty($kontakty['youtube'])) : ?>
+  <? if (isset($kontakty['whatsapp']) && !empty($kontakty['whatsapp'])) : ?>
+    <a class="icon icon-vk sp-sd" target="_blank" href="<?= $kontakty['whatsapp'] ?>">
+      <img src="/icon/what.svg" alt="">
+      <img src="/vidget/iocn/wh.svg" alt="" class="hover">
+    </a>
+  <? endif; ?>
+
+  <? if (isset($kontakty['telegram']) && !empty($kontakty['telegram'])) : ?>
+    <a class="icon icon-vk sp-sd" target="_blank" href="<?= $kontakty['telegram'] ?>">
+      <img src="/icon/tlg.svg" alt="">
+      <img src="/vidget/iocn/tg.svg" alt="" class="hover">
+    </a>
+  <? endif; ?>
+</div>
+<div class="item social">
+  <? if (isset($kontakty['youtube']) && !empty($kontakty['youtube'])) : ?>
     <a class="icon icon-youtube sp-sd" target="_blank" href="<?= $kontakty['youtube'] ?>">
       <img src="/icon/YoutubeMain.svg" alt="">
       <img src="/icon/hover/you.svg" alt="" class="hover">
@@ -25,30 +40,18 @@
   <? endif; ?>
 
   <? if (isset($kontakty['vkontakte']) && !empty($kontakty['vkontakte'])) : ?>
-    <a class="icon icon-vk sp-sd" target="_blank" href="<?= $kontakty['vkontakte'] ?>" >
+    <a class="icon icon-vk sp-sd" target="_blank" href="<?= $kontakty['vkontakte'] ?>">
       <img src="/icon/vkicon.svg" alt="">
       <img src="/icon/vk.svg" alt="" class="hover">
     </a>
   <? endif; ?>
 
-  <? if (isset($kontakty['whatsapp']) && !empty($kontakty['whatsapp'])) : ?>
-    <a class="icon icon-vk sp-sd" target="_blank" href="<?= $kontakty['whatsapp'] ?>" >
-      <img src="/icon/what.svg" alt="">
-      <img src="/vidget/iocn/wh.svg" alt="" class="hover">
-    </a>
-  <? endif; ?>
 
-  <? if (isset($kontakty['telegram']) && !empty($kontakty['telegram'])) : ?>
-    <a class="icon icon-vk sp-sd" target="_blank" href="<?= $kontakty['telegram'] ?>" >
-      <img src="/icon/tlg.svg" alt="">
-      <img src="/vidget/iocn/tg.svg" alt="" class="hover">
-    </a>
-  <? endif; ?>
-  
+
 </div>
 
 <div class="item  comcol">
-<? if (isset($kontakty['email']) && !empty($kontakty['email'])) : ?>
+  <? if (isset($kontakty['email']) && !empty($kontakty['email'])) : ?>
     <a class="icon icon-mail sp-sd" href="mailto:<?= $kontakty['email'] ?>">
       <img src="/icon/mailIconMain.svg" alt="">
       <img src="/icon/hover/mail.svg" alt="" class="hover">
