e3044bd975eb77a6da65e7f66380471bc3fb4d57
anton19ds <bar2018ka@gmail.com>
2022-10-24 16:36:15 +0300

    33

--- controllers/HeadingController.php
+++ controllers/HeadingController.php
@@ -41,7 +41,6 @@ class HeadingController extends MainController
     // print_r($headingOption);
     
     // echo "</pre>";
-     echo $heading->col;
     if ($heading->col == '2') {
       return $this->render('indexTwoColnew', [
         'heading' => null,
