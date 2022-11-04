06251ea0fa5664862cc5b415f8ec80156d653531
anton19ds <bar2018ka@gmail.com>
2022-11-04 13:37:54 +0300

    menu heading

--- widgets/views/mob-menu-not-child.php
+++ widgets/views/mob-menu-not-child.php
@@ -43,7 +43,7 @@
 <? if (stristr($item['id'], '_', true) == 'heading') : ?>
   <? $elem = $pages->findHeadingId($item['id']) ?>
   <? $imgB = $pages->findImg($item['id']); ?>
-  <li class="main-menu-item menu-item" data-color="<?= (isset($imgB) && !empty($imgB['color']) ? $imgB['color'] : '#759523') ?>">
+  <li class="main-menu-item menu-item heading-menu" data-color="<?= (isset($imgB) && !empty($imgB['color']) ? $imgB['color'] : '#759523') ?>">
     <a href="/<?= (isset($lang) && !empty($lang) ? $lang . '/pages/' : 'pages/') ?><?= (isset($elem['option']['link']) && ($elem['option']['link']) ? $elem['option']['link'] : '') ?>" class="menu-link main-menu-link" data-color="<?= (isset($imgB) && !empty($imgB['color']) ? $imgB['color'] : '#759523') ?>">
       <? if (isset($imgB['link']) && !empty($imgB['link'])) : ?>
         <div class="menu-icon" style="background-image: url(/icon/<?= $imgB['link'] ?>)"></div>
